1fba13085db8ef53b289d3f0965c247f3e53af80
Lakshyaraj Dash <195118589+codewithlaksh@users.noreply.github.com>
2025-05-08 06:44:19 +0530

    Video 11 Completed

--- src/lib/render-email-template.js
+++ src/lib/render-email-template.js
@@ -1,8 +1,13 @@
 import { render } from "@react-email/components";
 import VerificationEmail from "@/emails/verification-email";
+import ResetPasswordEmail from "@/emails/reset-password-email";
 
 const renderVerificationEmailTemplate = async (username, token) => {
     return render(<VerificationEmail username={username} token={token} />)
 }
 
-export { renderVerificationEmailTemplate };
+const renderResetPasswordEmailTemplate = async (username, token) => {
+    return render(<ResetPasswordEmail username={username} token={token} />)
+}
+
+export { renderVerificationEmailTemplate, renderResetPasswordEmailTemplate };
