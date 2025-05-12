ba027f99e2bd9cc7add4edb4cbc6026fb4b3e7ae
Lakshyaraj Dash <195118589+codewithlaksh@users.noreply.github.com>
2025-05-12 18:15:23 +0530

    Video 16 Completed

---
+++ src/app/(user)/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "My Profile | NextJS Authentication"
+}
+
+export default function ProfileLayout({ children }) {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
